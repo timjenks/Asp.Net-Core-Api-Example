ad019c03d2eff1368d9ec0e451aad51bc83d47a6
jonsteinn <jonsteinn@gmail.com>
2018-02-27 16:26:44 +0000

    entities

---
+++ TodoApi/Models/EntityModels/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace TodoApi.Models.EntityModels
+{
+    /// <summary>
+    /// The user as stored in the database.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        /// <summary>
+        /// No argument constructor.
+        /// </summary>
+        public ApplicationUser() { }
+
+        /// <summary>
+        /// The name of the user (UserName field is used for email).
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A colletion of all todos belonging to this user.
+        /// </summary>
+        public ICollection<Todo> Todos { get; set; }
+    }
+}
---
+++ TodoApi/Models/EntityModels/Todo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TodoApi.Models.EntityModels
+{
+    /// <summary>
+    /// Todo as stored in the database.
+    /// </summary>
+    public class Todo
+    {
+        /// <summary>
+        /// No argument constructor.
+        /// </summary>
+        public Todo() { }
+
+        /// <summary>
+        /// A unique identifier for todos.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// A time limit to complete the todo.
+        /// </summary>
+        public DateTime Due { get; set; }
+
+        /// <summary>
+        /// A description of what to do.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The owner of this todo.
+        /// </summary>
+        public ApplicationUser Owner { get; set; }
+    }
+}
