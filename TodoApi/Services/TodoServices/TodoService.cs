ec1e0ed9a396331aab07152dfaadf99507b3ca38
jonsteinn <jonsteinn@gmail.com>
2018-02-27 18:58:13 +0000

    post todo works

--- TodoApi/Services/TodoServices/TodoService.cs
+++ TodoApi/Services/TodoServices/TodoService.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoApi.Data;
+using TodoApi.Models.EntityModels;
 using TodoApi.Models.ViewModels;
 
 namespace TodoApi.Services.TodoServices
 {
     public class TodoService : ITodoService
     {
-        public Task<int> CreateTodoAsync(CreateTodoViewModel todo)
+        private readonly AppDataContext _db;
+
+        public TodoService(AppDataContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
+        }
+
+        public async Task<int> CreateTodoAsync(CreateTodoViewModel todo)
+        {
+            var newTodo = new Todo
+            {
+                Due = todo.Due.Value,
+                Description = todo.Description
+            };
+            await _db.AddAsync(newTodo);
+            await _db.SaveChangesAsync();
+            return newTodo.Id;
         }
     }
 }
