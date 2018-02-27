f463257cf070dcec9486b84a184e135623a41a5c
jonsteinn <jonsteinn@gmail.com>
2018-02-27 17:35:09 +0000

    controller init

---
+++ TodoApi/Constants/Routes.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoApi.Constants
+{
+    /// <summary>
+    /// A static class containing routes prefixes. This is done to simplify
+    /// support for multiple versions, so that each branch can change the
+    /// version at one place.
+    /// </summary>
+    public class Routes
+    {
+        /// <summary>
+        /// The route prefix for all controllers.
+        /// </summary>
+        private const string VersionPrefix = "api/v1/";
+
+        /// <summary>
+        /// The route prefix for TodoController.
+        /// </summary>
+        public const string TodoRoute = VersionPrefix + "todo";
+    }
+}
---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Constants;
+
+namespace TodoApi.Controllers
+{
+    [Route(Routes.TodoRoute)]
+    public class TodoController : Controller
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> Test()
+        {
+            return Ok();
+        }
+    }
+}
