f2d2c02f13dbec92d79fba638ff78010f405d749
jonsteinn <jonsteinn@gmail.com>
2018-03-03 16:34:01 +0000

    dto mock data

---
+++ Tests/MockData/DtoModels/MockApplicationUserDto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApi.Models.DtoModels;
+
+namespace Tests.MockData.DtoModels
+{
+    /// <summary>
+    /// A static coollection for mock user dtos.
+    /// </summary>
+    public static class MockApplicationUserDto
+    {
+        /// <summary>
+        /// Array of Mock data.
+        /// </summary>
+        private static readonly ApplicationUserDto[] Data =
+        {
+            // Index: 0
+            new ApplicationUserDto
+            {
+                Id = "0d31cf64-ad31-4f72-9776-036596c08e56",
+                Email = "megas@a-bleikum-nattkjolum.is",
+                Name = "Megas"
+            },
+            // Index: 1
+            new ApplicationUserDto
+            {
+                Id = "ea177560-e4ed-4af8-92a9-404d0344b741",
+                Email = "theyr@mjotvidur-maer.is",
+                Name = "Theyr"
+            },
+        };
+
+        /// <summary>
+        /// Get a single mock user dto. The instance is copied from the array.
+        /// </summary>
+        /// <param name="index">The index of the mock user dto to get</param>
+        /// <exception cref="ArgumentException">Thrown when index is out of bounds</exception>
+        /// <returns>The mock user dto with given index in array</returns>
+        public static ApplicationUserDto Get(int index)
+        {
+            if (index < 0 || index >= Data.Length)
+            {
+                throw new ArgumentException("You are accessing test data that does not exist");
+            }
+            return new ApplicationUserDto
+            {
+                Id = string.Copy(Data[index].Id),
+                Name = string.Copy(Data[index].Name),
+                Email = string.Copy(Data[index].Email)
+            };
+        }
+
+        /// <summary>
+        /// Get some mock user dtos. It will only contain copied instances.
+        /// </summary>
+        /// <param name="indices">Which users we want</param>
+        /// <returns>A list with some mocked user dtos</returns>
+        public static IEnumerable<ApplicationUserDto> GetSome(params int[] indices)
+        {
+            var copyList = new List<ApplicationUserDto>(indices.Length);
+            foreach (var index in indices)
+            {
+                copyList.Add(Get(index));
+            }
+            return copyList;
+        }
+
+        /// <summary>
+        /// Get all mock user dtos. It will only contain copied instances. 
+        /// </summary>
+        /// <returns>A list with every mocked user dtos</returns>
+        public static IEnumerable<ApplicationUserDto> GetAll()
+        {
+            var copyList = new List<ApplicationUserDto>(Data.Length);
+            for (var i = 0; i < Data.Length; i++)
+            {
+                copyList.Add(Get(i));
+            }
+            return copyList;
+        }
+    }
+}
---
+++ Tests/MockData/DtoModels/MockTodoDto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApi.Models.DtoModels;
+
+namespace Tests.MockData.DtoModels
+{
+    /// <summary>
+    /// A static coollection for mock todo dtos.
+    /// </summary>
+    public static class MockTodoDto
+    {
+        /// <summary>
+        /// Array of Mock data.
+        /// </summary>
+        private static readonly TodoDto[] Data =
+        {
+            // Index: 0
+            new TodoDto
+            {
+                Id = 0,
+                Due = new DateTime(0, 0, 0, 0, 0, 0),
+                Description = ""
+            },
+            // Index: 1
+            new TodoDto
+            {
+                Id = 0,
+                Due = new DateTime(0, 0, 0, 0, 0, 0),
+                Description = ""
+            }
+        };
+
+        /// <summary>
+        /// Get a single mock todo dto. The instance is copied from the array.
+        /// </summary>
+        /// <param name="index">The index of the mock todo dto to get</param>
+        /// <exception cref="ArgumentException">Thrown when index is out of bounds</exception>
+        /// <returns>The mock todo dto with given index in array</returns>
+        public static TodoDto Get(int index)
+        {
+            if (index < 0 || index >= Data.Length)
+            {
+                throw new ArgumentException("You are accessing test data that does not exist");
+            }
+            return new TodoDto
+            {
+                Id = Data[index].Id,
+                Due = Data[index].Due,
+                Description = string.Copy(Data[index].Description)
+            };
+        }
+
+        /// <summary>
+        /// Get some mock todo dtos. It will only contain copied instances.
+        /// </summary>
+        /// <param name="indices">Which todos we want</param>
+        /// <returns>A list with some mocked todo dtos</returns>
+        public static IEnumerable<TodoDto> GetSome(params int[] indices)
+        {
+            var copyList = new List<TodoDto>(indices.Length);
+            foreach (var index in indices)
+            {
+                copyList.Add(Get(index));
+            }
+            return copyList;
+        }
+
+        /// <summary>
+        /// Get all mock todo dtos. It will only contain copied instances. 
+        /// </summary>
+        /// <returns>A list with every mocked todo dtos</returns>
+        public static IEnumerable<TodoDto> GetAll()
+        {
+            var copyList = new List<TodoDto>(Data.Length);
+            for (var i = 0; i < Data.Length; i++)
+            {
+                copyList.Add(Get(i));
+            }
+            return copyList;
+        }
+    }
+}
