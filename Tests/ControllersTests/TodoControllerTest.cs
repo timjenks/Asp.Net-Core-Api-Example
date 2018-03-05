b29d94aaaab3a15578a45775cab195931ad0dcf0
jonsteinn <jonsteinn@gmail.com>
2018-03-05 21:20:54 +0000

    todo controller get single tests

--- Tests/ControllersTests/TodoControllerTest.cs
+++ Tests/ControllersTests/TodoControllerTest.cs
@@ -1,10 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Tests.MockData.Controllers;
+using Tests.MockData.DtoModels;
+using Tests.MockData.EntityModels;
+using Tests.MockData.Services;
+using TodoApi.Controllers;
+using TodoApi.Exceptions;
+using TodoApi.Models.DtoModels;
+using Xunit;
 
 namespace Tests.ControllersTests
 {
-    class TodoControllerTest
+    public class TodoControllerTest
     {
+        [Fact]
+        public async Task GetTodo_NonExisting_TodoNotFoundException()
+        {
+            // Arrange
+            var service = new MockTodoService
+            {
+                MGetTodoByIdAsync = (todoId, userId) => throw new TodoNotFoundException()
+            };
+            var controller = new TodoController(service);
+            MockClaims.AddUserIdClaim(controller, "I am just gonna throw an exception anyway");
+            var tId = 665;
+
+            // Act
+            var result = await controller.GetTodo(tId) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(404, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetTodo_Existing_OkWithDto()
+        {
+            // Arrange
+            var service = new MockTodoService
+            {
+                MGetTodoByIdAsync = (todoId, userId) => MockTodoDto.Get(0)
+            };
+            var controller = new TodoController(service);
+            MockClaims.AddUserIdClaim(controller, MockApplicationUsers.Get(5).Id);
+            var tId = 10101;
+
+            // Act
+            var result = await controller.GetTodo(tId) as OkObjectResult;
+            var dto = result.Value as TodoDto;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.NotNull(dto);
+            Assert.Equal(MockTodoDto.Get(0).Id, dto.Id);
+            Assert.Equal(MockTodoDto.Get(0).Due, dto.Due);
+            Assert.Equal(MockTodoDto.Get(0).Description, dto.Description);
+        }
     }
 }
---
+++ Tests/MockData/Controllers/MockClaims.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Tests.MockData.Controllers
+{
+    /// <summary>
+    /// A static class holding methods to add claims to controllers.
+    /// </summary>
+    public static class MockClaims
+    {
+        /// <summary>
+        /// Add a user id claim to a controller.
+        /// </summary>
+        /// <param name="controller">A controller to add claim to</param>
+        /// <param name="userId">The user id to add</param>
+        public static void AddUserIdClaim(Controller controller, string userId)
+        {
+            var mockContext = new Mock<HttpContext>(MockBehavior.Strict);
+            mockContext
+                .SetupGet(hc => hc.User.Claims)
+                .Returns(new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId)
+                });
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = mockContext.Object
+            };
+        }
+    }
+}
