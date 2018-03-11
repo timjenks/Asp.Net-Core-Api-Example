08130c3c9025295a6bd892d36ace8911da6283c3
jonsteinn <jonsteinn@gmail.com>
2018-03-11 14:14:32 +0000

    seed update

--- TodoApi/Data/Seed.cs
+++ TodoApi/Data/Seed.cs
@@ -18,15 +18,17 @@ namespace TodoApi.Data
     /// </summary>
     public static class Seed
     {
+        /// <summary>
+        /// Seeds the database with starting data. 
+        /// </summary>
+        /// <param name="serviceProvider">An injected service provider</param>
         public static async Task CreateAll(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
             var context = serviceProvider.GetRequiredService<AppDataContext>();
 
-#if DEBUG
             await ClearDatabase(roleManager, userManager, context);
-#endif
             await FillDatabase(roleManager, userManager, context);
         }
 
@@ -34,19 +36,32 @@ namespace TodoApi.Data
 
         #region Database
 
+        /// <summary>
+        /// Fills the database.
+        /// </summary>
+        /// <param name="roleManager">A role manager</param>
+        /// <param name="userManager">An user manager</param>
+        /// <param name="context">a db context</param>
         private static async Task FillDatabase(RoleManager<IdentityRole> roleManager, 
             UserManager<ApplicationUser> userManager, AppDataContext context)
         {
             await CreateRoles(roleManager);
-            var users = await CreateUsers(userManager);
 #if DEBUG
+            var users = await CreateUsers(userManager);
             await CreateTodo(context, users);
 #endif
         }
 
+        /// <summary>
+        /// Empty the database.
+        /// </summary>
+        /// <param name="roleManager">A role manager</param>
+        /// <param name="userManager">An user manager</param>
+        /// <param name="context">a db context</param>
         private static async Task ClearDatabase(RoleManager<IdentityRole> roleManager, 
             UserManager<ApplicationUser> userManager, AppDataContext context)
         {
+#if DEBUG
             context.RemoveRange(await context.Todo.ToListAsync());
             await context.SaveChangesAsync();
             foreach (var user in await userManager.Users.ToListAsync())
@@ -59,12 +74,17 @@ namespace TodoApi.Data
                 await roleManager.DeleteAsync(role);
             }
             await context.SaveChangesAsync();
+#endif
         }
 
         #endregion
 
         #region Roles
 
+        /// <summary>
+        /// Create all roles.
+        /// </summary>
+        /// <param name="roleManager">A role manager</param>
         private static async Task CreateRoles(RoleManager<IdentityRole> roleManager)
         {
             foreach (var roleName in new[] { Roles.User, Roles.Admin })
@@ -73,6 +93,11 @@ namespace TodoApi.Data
             }
         }
 
+        /// <summary>
+        /// Create a single role.
+        /// </summary>
+        /// <param name="roleManager">A role manager</param>
+        /// <param name="roleName">The name of the role to create</param>
         private static async Task CreateRole(RoleManager<IdentityRole> roleManager, string roleName)
         {
             if (!(await roleManager.RoleExistsAsync(roleName)))
@@ -81,34 +106,37 @@ namespace TodoApi.Data
             }
         }
 
-#endregion
+        #endregion
 
         #region Users
 
+        /// <summary>
+        /// Create all applicaion users.
+        /// </summary>
+        /// <param name="userManager">An user manager</param>
+        /// <returns>A dictionary that maps emails to their application users</returns>
         private static async Task<Dictionary<string, ApplicationUser>> CreateUsers(UserManager<ApplicationUser> userManager)
         {
-            var dictionary = new Dictionary<string, ApplicationUser>
+            return new Dictionary<string, ApplicationUser>
             {
-                { "kong@mail.com", await CreateAdmin(userManager, "King Kong", "kong@mail.com", "D0nkey_K0ng") }
+                { "kong@mail.com", await CreateUser(userManager, "King Kong", "kong@mail.com", "D0nkey_K0ng", Roles.Admin) },
+                { "tyrion@mail.com", await CreateUser(userManager, "Tyrion Lannister", "tyrion@mail.com", "D0nkey_K0ng", Roles.User) },
+                { "jonsteinn@gmail.com", await CreateUser(userManager, "Jón Steinn Elíasson", "jonsteinn@gmail.com", "D0nkey_K0ng", Roles.User) }
             };
-#if DEBUG
-            dictionary.Add
-            (
-                "tyrion@mail.com", 
-                await CreateUser(userManager, "Tyrion Lannister", "tyrion@mail.com", "D0nkey_K0ng")
-            );
-            dictionary.Add
-            (
-                "jonsteinn@gmail.com", 
-                await CreateUser(userManager, "Jón Steinn Elíasson", "jonsteinn@gmail.com", "D0nkey_K0ng")
-            );
-#endif
-            return dictionary;
         }
 
+        /// <summary>
+        /// Create a single ApplicationUser in the User role.
+        /// </summary>
+        /// <param name="userManager">An user manager</param>
+        /// <param name="name">The name of the user to create</param>
+        /// <param name="email">The email of the user to create</param>
+        /// <param name="password">The password of the user to create</param>
+        /// <param name="role">The role of the user to create</param>
+        /// <returns>The entity of the user created</returns>
         private static async Task<ApplicationUser> CreateUser(
             UserManager<ApplicationUser> userManager,
-            string name, string email, string password)
+            string name, string email, string password, string role)
         {
             var findUser = await userManager.FindByEmailAsync(email);
             if (findUser != null) return findUser;
@@ -121,27 +149,7 @@ namespace TodoApi.Data
             var result = await userManager.CreateAsync(user, password);
             if (result.Succeeded)
             {
-                await userManager.AddToRoleAsync(user, Roles.User);
-            }
-            return user;
-        }
-
-        private static async Task<ApplicationUser> CreateAdmin(
-            UserManager<ApplicationUser> userManager,
-            string name, string email, string password)
-        {
-            var findUser = await userManager.FindByEmailAsync(email);
-            if (findUser != null) return findUser;
-            var user = new ApplicationUser()
-            {
-                Name = name,
-                UserName = email,
-                Email = email
-            };
-            var result = await userManager.CreateAsync(user, password);
-            if (result.Succeeded)
-            {
-                await userManager.AddToRoleAsync(user, Roles.Admin);
+                await userManager.AddToRoleAsync(user, role);
             }
             return user;
         }
@@ -150,6 +158,11 @@ namespace TodoApi.Data
 
         #region Todo
 
+        /// <summary>
+        /// Create all todos.
+        /// </summary>
+        /// <param name="context">a db context</param>
+        /// <param name="users">The dictionary of emails to users entity</param>
         private static async Task CreateTodo(AppDataContext context, Dictionary<string, ApplicationUser> users)
         {
             await CreateTodo
@@ -197,6 +210,13 @@ namespace TodoApi.Data
             await context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Create a single todo.
+        /// </summary>
+        /// <param name="context">a db context</param>
+        /// <param name="owner">The entity of the owner</param>
+        /// <param name="description">The tood's description</param>
+        /// <param name="due">The due date of the todo</param>
         private static async Task CreateTodo(AppDataContext context,
             ApplicationUser owner, string description, DateTime due)
         {
